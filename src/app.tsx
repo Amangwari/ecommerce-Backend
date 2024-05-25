f605d144d0018c8eb47b512525651b395731893b
Aman gwari <amangwari29sep@gmail.com>
2024-05-26 00:30:17 +0530

    Initial commit, Setting up

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import express from 'express'
+
+
+
+//  Importing routes
+import userRotes from './routes/user.js'
+
+const port = 4000;
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("API Working")
+})
+
+// using routes
+app.use("/api/v1", userRotes)
+
+app.listen(port, () => {
+    console.log(`Server is working on http://localhost:${port}`);
+
+})
