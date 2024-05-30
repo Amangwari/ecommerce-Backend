0461341559354bb47ea6ca4cfda960399e9a2b73
Aman gwari <amangwari29sep@gmail.com>
2024-05-30 22:39:10 +0530

    .

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.js";
 import { newUserRequestBody } from "../types/types.js";
-
+ 
 export const newUser = async (
     // req: Request<{}, {}, newUserRequestBody>, as we import  (same as =>  newUserRequestBody  const { name, email, photo, gender, role, _id, dob } = req.body;) 
     req: Request,
